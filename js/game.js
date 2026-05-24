5524156644b9ed41a889a52df5566b404e2fd6e9
Ludvig Erdmann <ludvig.erdmann@gmail.com>
2026-05-24 22:17:23 +0200

    fixed play again btn

--- js/game.js
+++ js/game.js
@@ -3,4 +3,8 @@ function initGame() {
     populateBars()
     document.querySelector('#game #timer').textContent = 0
     document.querySelector('#game .bar > .girl > .avatar').setAttribute('src', '')
+    document.querySelector('#game #convo #ai-response span').textContent = ''
+    document.querySelectorAll('#game .bar.girl-selected').forEach(e => {
+        e.classList.remove('girl-selected')
+    });
 }
